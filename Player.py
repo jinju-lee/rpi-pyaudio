e16cb4f2ba28aecf630cf138458dfe59ed3a3726
Jinju Lee <35492329+jinju-lee@users.noreply.github.com>
2018-08-08 15:38:42 +0900

    play recording file

---
+++ Player.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from pytz import timezone
+from subprocess import call
+import os
+
+def main():
+    now = datetime.now(timezone('Asia/Seoul'))
+
+    while True:
+        name=str(input("file name: "))
+        name=os.getcwd()+'/'+now.strftime('%Y%m%d_')+name+'.wav'
+        call(["aplay", name])
+
+if __name__ == '__main__':
+    main()
