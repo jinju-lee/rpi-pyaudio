199dfbe18a32dee9566039609d347b29d07e4fbc
Jinju Lee <35492329+jinju-lee@users.noreply.github.com>
2018-08-08 17:43:47 +0900

    1초 이하 파일 거르기, 앞부분 노이즈 제거

--- Recorder.py
+++ Recorder.py
@@ -6,6 +6,9 @@ from aiy.vision.leds import Leds
 from datetime import datetime
 from pytz import timezone
 import RPi.GPIO as GPIO
+import soundfile as sf
+from subprocess import call
+#from pydub import AudioSegment
 
 #GPIO setting
 GPIO.setmode(GPIO.BCM)
@@ -25,11 +28,12 @@ def record_start():
     y = now.strftime('%Y%m%d_%H%M%S')
     filename = y + '.wav';
 
+    #Wav header setting
     CHUNK = 1024  # 1frame 당 1024개 buffer
-    FORMAT = pyaudio.paInt16  # 16-bit int형 signed
-    CHANNELS = 1  # mono/stereo
+    FORMAT = pyaudio.paInt16  # 16-bit signed int형 .
+    CHANNELS = 1  # mono
     RATE = 16000  # 16khz
-    RECORD_SECONDS = 2
+    #RECORD_SECONDS = 2
     WAVE_OUTPUT_FILENAME = filename
 
     p = pyaudio.PyAudio()
@@ -38,30 +42,25 @@ def record_start():
                     rate=RATE,
                     input=True,
                     frames_per_buffer=CHUNK)
-
+    #Start recording
     print("Start to record the audio.")
     frames = []
-
     while GPIO.input(_GPIO_BUTTON) == 0:
         leds.update(Leds.rgb_on(MAGENTA))
-        time.sleep(0.02)
+        time.sleep(0.05)
         data = stream.read(CHUNK)
         frames.append(data)
-
         # record stream
         if GPIO.input(_GPIO_BUTTON) == 1:
             leds.update(Leds.rgb_off())
             break
-
-    print("Recording is finished.")
-    leds.update(Leds.rgb_off())
-
     # stop stream
     stream.stop_stream()
     stream.close()
     # close PyAudio
     p.terminate()
 
+    #Save recording and close
     wo = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
     wo.setnchannels(CHANNELS)
     wo.setsampwidth(p.get_sample_size(FORMAT))
@@ -69,12 +68,25 @@ def record_start():
     wo.writeframes(b''.join(frames))
     wo.close()
 
+    #File length check
+    f = sf.SoundFile(filename)
+    file_len = len(f) / f.samplerate
+    print(file_len)
+    if file_len < 1:
+        print("Recording is stoped.")
+        call(["rm", filename])
+        main()
+    '''
+    song = AudioSegment.from_wav(filename)
+    new_file = song[:(file_len-0.002)]
+    new_file.export(filename, format="wav")
+    '''
+    print("Recording is finished.")
+    leds.update(Leds.rgb_off())
     print(WAVE_OUTPUT_FILENAME)
 
 def main():
     leds.update(Leds.rgb_on(BLUE))
-    time.sleep(1)
-
     while True:
         button.wait_for_press()
         record_start()
