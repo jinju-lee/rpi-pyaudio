2ae47c538404a401a6bcb3fe36a98bb78bdcaac8
Jinju Lee <35492329+jinju-lee@users.noreply.github.com>
2018-08-08 16:05:52 +0900

    Play recording files

--- Player.py
+++ Player.py
@@ -5,7 +5,6 @@ import os
 
 def main():
     now = datetime.now(timezone('Asia/Seoul'))
-
     while True:
         name=str(input("file name: "))
         name=os.getcwd()+'/'+now.strftime('%Y%m%d_')+name+'.wav'
